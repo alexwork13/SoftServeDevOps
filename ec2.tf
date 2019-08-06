d2e3769e944b7a5499b503b6fb883deea1bd8a48
alex <alexwork13@ukr.net>
2019-08-07 01:49:34 +0300

    added zip with lambda

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
-/*resource "aws_key_pair" "my-test-key" {
-  key_name   = "ubuntu"
-  public_key = ""
-} */
+resource "aws_key_pair" "my-test-key" {
+  key_name   = "ubuntu1"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTX6JKLZwjbRKsN4LrjlBVXXEMuhVpzObfo/LssxSyAp+8AuEp1w8+gVTqo8ekLs6QfnsUyuxCiQ0gBlVO39bzKT4RvIb3ME8jJ9cHT0sSFUajwCWPsrNjdupM9FDOg7KJKJGrKnKTYrcrhVB8vOY9GkwdgzgYFTvQbETDAQns94+Xq8jO8vUFeKAJGYpCcOBOJ7Oqn+K7HAXzn0utlWfgTMDWNIYfsxn4BLaCp84ovQ/Dk/lrPJLyEgO9ENCBHIvmpvYr7oyTKdYWEXn7I6/DPzSVTzDOQIhzv6w8SKjDpICf19DGGb9fvMbZN83owTLWkoZzat33Ygnjj0K5cGFH alex@HP"
+}
+
 
 
 resource "aws_instance" "my_webserver" {
@@ -11,7 +12,7 @@ resource "aws_instance" "my_webserver" {
   vpc_security_group_ids = [aws_security_group.public_sg.id]
   user_data              = file("user_data.sh")
   subnet_id              = "${aws_subnet.public1.id}"
-//  key_name               = "${aws_key_pair.my-test-key.key_name}"
+  key_name               = "${aws_key_pair.my-test-key.key_name}"
 
   tags = {
     Name  = "Web Server Build by Terraform"
